d9742a7786e3ba6c17c82391d72bdfe16b2e2707
SkollProg <dev@skollprog.com>
2018-03-06 15:18:47 -0300

    Update init.php

--- init.php
+++ init.php
@@ -2,7 +2,7 @@
 
 	require_once("clear-backup.php");
 	$backup = new clearBackup();
-	$backup->route("./") // Ruta padre de Updraft
+	$backup->route("./"); // Ruta padre de Updraft
 	$backup->purge();
 
 ?>
